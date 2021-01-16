062c2eab13016c176808307ca8ffe93c54ff4981
Jojo Trujillo <jojo@theruntime.org>
2021-01-16 17:06:18 +0000

    Update previous changes; 'ment out React templates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
   return (
-    <div id="quote-box">
-      <span id="text">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </span>
-      <span id="author">
-      </span>
-      <button id="new-quote">New quote</button>
+    <div id="wrapper">
+      {/*
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+      */}
+      <div id="quote-box">
+        <div class="quote-text">
+          <i class="fas fa-quote-left"> </i><span id="text"></span>
+        </div>
+        <div class="quote-author">- <span id="author"></span></div>
+        <div class="buttons">
+          <a
+            class="button"
+            id="tweet-quote"
+            title="Tweet this quote!"
+            target="_top"
+          >
+            <i class="fab fa-twitter"></i>
+          </a>
+          <button class="button" id="new-quote">New quote</button>
+        </div>
+      </div>
     </div>
   );
 }
